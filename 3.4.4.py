2f1fc913eba20beefe1ba8c84a2e42c366d36c11
beemaster1972 <ugor.finno@yandex.ru>
2023-09-12 17:13:52 +0500

    3.4.4

--- 3.4.4.py
+++ 3.4.4.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-with webdriver.Chrome() as browser:
+with webdriver.Firefox() as browser:
     browser.get('https://parsinger.ru/selenium/1/1.html')
-    input_form = browser.find_element(By.CLASS_NAME, 'form').send_keys('Text')
-    time.sleep(5)
+    input_forms = browser.find_elements(By.CLASS_NAME, 'form')
+    button = browser.find_element(By.CLASS_NAME, 'btn')
+    for i, form in enumerate(input_forms):
+        form.send_keys('Text')
+    button.click()
+    time.sleep(20)
